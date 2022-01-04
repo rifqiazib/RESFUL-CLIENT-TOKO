f2ad7eebf165b52a579f267441b29ecca183e5d5
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2022-01-05 01:15:29 +0700

    [hotfix] update consume API

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,15 +8,18 @@ class ClientController extends Controller
 {
     public function guzzleGet()
     {
+        $data = [];
         try {
             $client = new \GuzzleHttp\Client();
             $response = $client->get('http://localhost:3000/api/produk/');
             $body = $response->getBody();
             $body_array = json_decode($body);
             
-            dd($body_array);
+            $data['list_produk'] = $body_array->data;
         } catch(\Exception $error) {
             return $error->getMessage();
         }
+
+        return view('welcome', $data);
     }
 }
